49d958fa2658ef5f567e2221dee91ff2cd946811
Jan Moesen <github.com@moesen.nu>
2011-05-06 11:32:58 +0200

    Readable++: make SVG selector a bit more readable
    Ha, "more readable". How meta.

--- readable++.js
+++ readable++.js
@@ -24,7 +24,7 @@
 			font-style: normal;
 			text-decoration: none
 		}
-		img:not(:hover), input[type="image"]:not(:hover), object:not(:hover), embed:not(:hover), iframe:not(:hover), :not(svg|*)>svg|*:not(:hover) {
+		img:not(:hover), input[type="image"]:not(:hover), object:not(:hover), embed:not(:hover), iframe:not(:hover), :not(svg|*) > svg|*:not(:hover) {
 			opacity: 0.25;
 		}
 		.post_share {
