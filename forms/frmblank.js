777111a2f23cbe7f6c15f8238feff87cf6c17a14
Jan Moesen <github.com@moesen.nu>
2011-05-13 15:45:12 +0200

    Form GET/POST/_blank: move recursion into the try block
    I had moved it out for debugging, but forgot to to move the "try {" afterwards.

--- forms/frmblank.js
+++ forms/frmblank.js
@@ -9,10 +9,10 @@
 	});
 
 	/* Recurse for frames and iframes. */
+	try {
 		Array.prototype.slice.call(document.querySelectorAll('frame, iframe, object[type^="text/html"], object[type^="application/xhtml+xml"]')).forEach(function (elem) {
 			frmblank(elem.contentDocument);
 		});
-	try {
 	} catch (e) {
 		/* Catch exceptions for out-of-domain access, but do not do anything with them. */
 	}
