0e0d79a5c393a94bcbe5bc00376642db2bd5a7db
Jan Moesen <github.com@moesen.nu>
2011-05-14 17:31:33 +0200

    Link this: move title logic to actual assignment

--- navigation/link.js
+++ navigation/link.js
@@ -6,7 +6,7 @@
 (function link() {
 	var fragmentRoot = document.createDocumentFragment().appendChild(document.createElement('html')),
 	    fragmentTitle = fragmentRoot.appendChild(document.createElement('title')),
-	    titleText = (document.querySelector('title') && document.querySelector('title').textContent + ' [' + (document.domain || location) + ']') || location,
+	    titleText = document.querySelector('title') && document.querySelector('title').textContent || document.title,
 	    iconLink = document.querySelector('link[rel*="icon"]') || document.createElement('link');
 
 	/* Make sure the favicon HREF is absolute. If there was none, use Google S2. */
@@ -21,7 +21,7 @@
 	/* Link to the current page using its title. */
 	var link = fragmentRoot.appendChild(document.createElement('a'));
 	link.href = location;
-	link.textContent = fragmentTitle.textContent = titleText;
+	link.textContent = fragmentTitle.textContent = titleText ? titleText + ' [' + (document.domain || location) + ']' : location,
 
 	/* Open the data: URI with existing %XX encodings intact. */
 	document.location = 'data:text/html;charset=UTF-8,' + fragmentRoot.innerHTML.replace(/%/g, '$&' + 25);
