515342efe8ea53c74be61be13a8be2946e201064
Jan Moesen <github.com@moesen.nu>
2011-05-15 22:36:33 +0200

    Link this: limit link icon to 32px

--- navigation/link.js
+++ navigation/link.js
@@ -10,7 +10,7 @@
 	    domain = document.domain || location.hostname;
 
 	/* Build a basic HTML document for easy element access. */
-	root.innerHTML = '<head><title></title><link rel="icon"><style>html { font-family: "Calibri", sans-serif; }</style><body><p><img> <span><a></a></span><p>Link code:<br><textarea rows="10" cols="80"></textarea>';
+	root.innerHTML = '<head><title></title><link rel="icon"><style>html { font-family: "Calibri", sans-serif; } img { max-width: 32px; max-height: 32px; }</style><body><p><img> <span><a></a></span><p>Link code:<br><textarea rows="10" cols="80"></textarea>';
 	var title = root.querySelector('title'),
 	    iconLink = root.querySelector('link'),
 	    styleSheet = root.querySelector('style'),
