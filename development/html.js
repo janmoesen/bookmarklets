9cec2fcc3edf7c73b4b6daeceeebb3db99bde8bd
Jan Moesen <github.com@moesen.nu>
2011-05-16 22:05:57 +0200

    View as HTML: initial commit

---
+++ development/html.js
@@ -0,0 +1,12 @@
+javascript:
+/**
+ * Render the specified or selected text as HTML.
+ *
+ * @title View as HTML
+ */
+(function html() {
+	var s = (<><![CDATA[%s]]></> + '') || getSelection() + '' || prompt('Please enter your HTML snippet:');
+	if (s) {
+		location = 'data:text/html;charset=UTF-8,' + encodeURIComponent(s);
+	}
+})();
