aa0fbbb5fb8872c6d51dd4cc5c24d0e297a4a7f1
Jan Moesen <github.com@moesen.nu>
2011-05-17 21:58:48 +0200

    Readable++: find the content start before our CSS
    Because our stylesheet can unhide certain elements, false positives might
    be triggered, causing us to scroll to someplace unexpected. By looking for
    the start of the content when our CSS is not active, we get more accurate
    matches.
    
    This required moving the stylesheet toggling and content selector matching
    code into two separate functions. No biggie. (git diff -w makes it look
    less massive.)

--- read.js
+++ read.js
@@ -70,42 +70,84 @@ javascript:
 		}
 
 		/* Toggle between our readable and the page's original stylesheet(s). */
-		ourStyleSheet.disabled = !ourStyleSheet.disabled;
-		allStyleSheets.forEach(function (styleSheet, i) {
-			if (styleSheet.ownerNode !== ourStyleSheet)
-			{
-				/* Remember whether this stylesheet was originally disabled or not. We can't store on the CSSStyleSheet object, so use our DOM node. */
-				if (ourStyleSheet[id + '-originally-disabled-' + i] === undefined) {
-					ourStyleSheet[id + '-originally-disabled-' + i] = styleSheet.disabled;
-				}
+		function toggleStyles() {
+			ourStyleSheet.disabled = !ourStyleSheet.disabled;
+			allStyleSheets.forEach(function (styleSheet, i) {
+				if (styleSheet.ownerNode !== ourStyleSheet)
+				{
+					/* Remember whether this stylesheet was originally disabled or not. We can't store on the CSSStyleSheet object, so use our DOM node. */
+					if (ourStyleSheet[id + '-originally-disabled-' + i] === undefined) {
+						ourStyleSheet[id + '-originally-disabled-' + i] = styleSheet.disabled;
+					}
 
-				if (ourStyleSheet.disabled) {
-					/* Restore this stylesheet's original state. */
-					styleSheet.disabled = ourStyleSheet[id + '-originally-disabled-' + i];
-				} else {
-					/* Disable this stylesheet when ours is enabled. */
-					styleSheet.disabled = true;
-				}
-			}
-		});
-
-		/* Process all attributes for all elements. */
-		all.forEach(function (elem, i) {
-			attrs.forEach(function (attr, j) {
-				/* Parse the attribute definition. Attributes can be restricted to certain elements, e.g. "table@width". */
-				if (!(matches = attr.match(/([^@]+)@([^@]+)/)) || (elem.tagName && elem.tagName.toLowerCase() == matches[1])) {
-					attr = matches ? matches[2] : attr;
-					var names = { enabled: attr, disabled: id + '-' + attr };
-					if (elem.hasAttribute(names.enabled)) {
-						elem.setAttribute(names.disabled, elem.getAttribute(names.enabled));
-						elem.removeAttribute(names.enabled);
-					} else if (elem.hasAttribute(names.disabled)) {
-						elem.setAttribute(names.enabled, elem.getAttribute(names.disabled));
-						elem.removeAttribute(names.disabled);
+					if (ourStyleSheet.disabled) {
+						/* Restore this stylesheet's original state. */
+						styleSheet.disabled = ourStyleSheet[id + '-originally-disabled-' + i];
+					} else {
+						/* Disable this stylesheet when ours is enabled. */
+						styleSheet.disabled = true;
 					}
 				}
 			});
-		});
+
+			/* Process all attributes for all elements. */
+			all.forEach(function (elem, i) {
+				attrs.forEach(function (attr, j) {
+					/* Parse the attribute definition. Attributes can be restricted to certain elements, e.g. "table@width". */
+					if (!(matches = attr.match(/([^@]+)@([^@]+)/)) || (elem.tagName && elem.tagName.toLowerCase() == matches[1])) {
+						attr = matches ? matches[2] : attr;
+						var names = { enabled: attr, disabled: id + '-' + attr };
+						if (elem.hasAttribute(names.enabled)) {
+							elem.setAttribute(names.disabled, elem.getAttribute(names.enabled));
+							elem.removeAttribute(names.enabled);
+						} else if (elem.hasAttribute(names.disabled)) {
+							elem.setAttribute(names.enabled, elem.getAttribute(names.disabled));
+							elem.removeAttribute(names.disabled);
+						}
+					}
+				});
+			});
+		}
+
+		/* Find the first thing that looks like the start of the actual content so we can scroll to it after applying our CSS. */
+		function findContentElement() {
+			if (location.hash) {
+				contentSelectors.unshift('a[name="' + location.hash.substring(1) + '"]');
+				contentSelectors.unshift(location.hash);
+			}
+			for (var i = 0; i < contentSelectors.length; i++) {
+				try {
+					var element = document.querySelector(contentSelectors[i]);
+					/* Make sure the element was either an anchor or something "visible". */
+					if (element && (element.tagName.toLowerCase() === 'a' || (element.offsetWidth && element.offsetHeight))) {
+						window.console && console.log('Readable++: matching selector: ' + contentSelectors[i] + '\nElement: ', element);
+						return element;
+					}
+				}
+				catch (e) {
+					window.console && console.log('Readable++: bad selector: ' + contentSelectors[i] + '\nException: ' + e);
+				}
+			}
+		}
+
+		/* Toggle the stylesheets and find the start of the content. */
+		var contentElement;
+		if (ourStyleSheet.disabled) {
+			/* Look for the content element when our CSS is NOT active to avoid scrolling to hidden elements being unhidden by our CSS (and thus causing false positives). */
+			contentElement = findContentElement();
+			toggleStyles();
+		} else {
+			toggleStyles();
+			contentElement = findContentElement();
+		}
+		/* Scroll to the start of the content after the stylesheets have been toggled. */
+		if (contentElement) {
+			var top = 0;
+			do {
+				top += contentElement.offsetTop;
+			} while ((contentElement = contentElement.offsetParent));
+			window.scrollTo(0, top);
+		}
 
 		/* Recurse for frames and iframes. */
 		try {
@@ -116,29 +158,4 @@ javascript:
 			/* Catch exceptions for out-of-domain access, but do not do anything with them. */
 		}
 	})(document);
-
-	/* Scroll to the first thing that looks like the start of the actual content. */
-	if (location.hash) {
-		contentSelectors.unshift('a[name="' + location.hash.substring(1) + '"]');
-		contentSelectors.unshift(location.hash);
-	}
-	for (var i = 0; i < contentSelectors.length; i++) {
-		try {
-			var element = document.querySelector(contentSelectors[i]);
-			/* Make sure the element was either an anchor or something "visible". */
-			if (element && (element.tagName.toLowerCase() === 'a' || (element.offsetWidth && element.offsetHeight))) {
-				var elementSelector = element.tagName.toLowerCase() + (element.id ? '#' + element.id : '') + (element.className ? '.' + element.className.replace(/\s/g, '.') : '');
-				var top = 0;
-				do {
-					top += element.offsetTop;
-				} while ((element = element.offsetParent));
-				window.console && console.log('Readable++: matching selector: ' + contentSelectors[i] + '\nScrolling to ' + elementSelector + ' at (0, ' + top + ')');
-				window.scrollTo(0, top);
-				break;
-			}
-		}
-		catch (e) {
-			window.console && console.log('Readable++: bad selector: ' + contentSelectors[i] + '\nException: ' + e);
-		}
-	}
 })();
