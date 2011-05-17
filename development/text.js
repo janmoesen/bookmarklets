3c53f9dc7b898a228c6d527f5098cc4e2ee1ab72
Jan Moesen <github.com@moesen.nu>
2011-05-17 22:34:09 +0200

    View as text: initial commit
    Copy-pasta from "View as HTML", obviously.

---
+++ development/text.js
@@ -0,0 +1,12 @@
+javascript:
+/**
+ * Render the specified or selected text as plain text.
+ *
+ * @title View as text
+ */
+(function text() {
+	var s = (<><![CDATA[%s]]></> + '') || getSelection() + '' || prompt('Please enter your text snippet:');
+	if (s) {
+		location = 'data:text/plain;charset=UTF-8,' + encodeURIComponent(s);
+	}
+})();
