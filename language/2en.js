2fb9d515f9b3f7c04b5179b5ab5c10f8107b5aa9
Jan Moesen <github.com@moesen.nu>
2011-05-19 22:34:23 +0200

    Translate to English: initial commit

---
+++ language/2en.js
@@ -0,0 +1,15 @@
+javascript:
+/**
+ * Translate the specified or selected text to English using Google.
+ *
+ * @title Translate to English
+ * @keyword 2en
+ */
+(function () {
+	var s = (<><![CDATA[%s]]></> + '') || getSelection() + '' || location + '';
+	if (s) {
+		location = s.match(/^(\w+:(\/\/)?)?[^\s]+(\.[^\s])+/)
+			? 'http://translate.google.com/translate?sl=auto&tl=en&u=' + encodeURIComponent(s)
+			: 'http://translate.google.com/translate_t#auto|en|' + encodeURIComponent(s);
+	}
+})();
