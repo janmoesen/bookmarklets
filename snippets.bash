09412479568b932bcfd43e31c200268e86cfb781
Jan Moesen <github.com@moesen.nu>
2011-05-19 22:35:04 +0200

    snippets.bash: initial commit
    Small code snippets to help during development.

---
+++ snippets.bash
@@ -0,0 +1,21 @@
+# Short snippets to run on the command line.
+echo 'This is not meant to be run as a stand-alone script. Copy and paste.';
+exit 1;
+
+# Copy the last modified bookmarklet to the clipboard.
+# Cygwin/Linux users might want to use "putclip"/"xclip" or something similar.
+PUTCLIP=; pbcopy < <(file="$(find . -name '*.js' -exec ls -1rt {} + | tail -n 1)" && echo "Copied $file to the clipboard." 1>&2 && cat "$file");
+
+# Copy the Google Translate bookmarklet from English to some other languages.
+copy-2en () {
+	while [ $# -ge 2 ]; do
+		lang="$1";
+		name="$2";
+		shift;
+		shift;
+		perl -p -e "s/(2|\b)en\b/\$1$lang/g; s/English/$name/g" language/2en.js > "language/2$lang.js" \
+			&& echo "Copied to $name" \
+			|| echo "Failed to copy to $name";
+	done;
+}
+COPY_2EN=; copy-2en nl Dutch fr French de German it Italian es Spanish
