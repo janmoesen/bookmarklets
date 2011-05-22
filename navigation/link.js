722408bb3e68bfaab2ca556c04a817504f8277dd
Jan Moesen <github.com@moesen.nu>
2011-05-22 21:17:29 +0200

    Link this: *properly* trim the title

--- navigation/link.js
+++ navigation/link.js
@@ -5,7 +5,7 @@
  */
 (function link() {
 	var root = document.createDocumentFragment().appendChild(document.createElement('html')),
-	    titleText = (document.querySelector('title') && document.querySelector('title').textContent || document.title).replace(/^\s*(.*)\s*$/, '$1'),
+	    titleText = (document.querySelector('title') && document.querySelector('title').textContent || document.title).replace(/\n/g, ' ').replace(/^\s*(.*?)\s*$/g, '$1'),
 	    originalIconLink = document.querySelector('link[rel*="icon"]'),
 	    domain = document.domain || location.hostname;
 
