8441ec28c7e195157c465a4c446ad2b4ae7ea899
Jan Moesen <github.com@moesen.nu>
2011-05-27 23:53:40 +0200

    img: initial commit

---
+++ search/img.js
@@ -0,0 +1,58 @@
+/**
+ * Search Google Images, optionally limiting to certain sizes.
+ *
+ * @title Google Images
+ */
+(function img() {
+	var s = (<><![CDATA[%s]]></> + '').replace(/\u0025s/, '') || getSelection() + '' || prompt('Please enter your query:');
+	if (s) {
+		var url = 'http://images.google.com/images?safe=off&biw=' + window.innerWidth + '&bih=' + window.innerHeight;
+		var words = s.split(' '), matches;
+		if (words.length > 1) {
+			if ((matches = words[0].match(/^=?([0-9]+)x([0-9]+)$/))) {
+				/* Exact size requested. */
+				url += '&tbs=isz:ex,iszw:' + encodeURIComponent(matches[1]) + ',iszh:' + encodeURIComponent(matches[2]);
+				words.shift();
+			} else if ((matches = words[0].match(/^>(([1-9][0-9]*)x([1-9][0-9]*)|qsvga|vga|svga|xga|(?:([1-9][0-9]*)mp))$/))) {
+				var minWidth = matches[2] * 1, minHeight = matches[3] * 1, minMegapixels = matches[4] * 1;
+				/* "Larger than X" requested. */
+				var resolutions = {
+					'400x300': 'qsvga',
+					'640x480': 'vga',
+					'800x600': 'svga',
+					'1024x768': 'xga',
+					'1600x1200': '2mp',
+					'2272x1704': '4mp',
+					'2816x2112': '6mp',
+					'3264x2448': '8mp',
+					'3648x2736': '10mp',
+					'4096x3072': '12mp',
+					'4480x3360': '15mp',
+					'5120x3840': '20mp',
+					'7216x5412': '40mp',
+					'9600x7200': '70mp'
+				};
+				var size = matches[1];
+				if (minWidth && minHeight) {
+					for (var dimensions in resolutions) {
+						var width = dimensions.split('x')[0] * 1, height = dimensions.split('x')[1] * 1;
+						if (minWidth >= width && minHeight >= height) {
+							size = resolutions[dimensions];
+						}
+					}
+				} else if (minMegapixels) {
+					for (var dimensions in resolutions) {
+						var megapixels = parseInt(resolutions[dimensions], 10);
+						if (minMegapixels >= megapixels) {
+							size = resolutions[dimensions];
+						}
+					}
+				}
+				url += '&tbs=isz:lt,islt:' + encodeURIComponent(size);
+				words.shift();
+			}
+			s = words.join(' ');
+		}
+		location = url + '&q=' + encodeURIComponent(s);
+	}
+})();
