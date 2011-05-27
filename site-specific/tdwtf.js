1bac69d7c84573e366094cf954b1e2078e93c01c
Jan Moesen <github.com@moesen.nu>
2011-05-28 00:02:57 +0200

    TDWTF: initial commit

---
+++ site-specific/tdwtf.js
@@ -0,0 +1,23 @@
+/**
+ * Fix the forum for The Daily WTF after having applied Readable++.
+ *
+ * @title TDWTF
+ */
+(function tdwtf() {
+	Array.prototype.slice.call(document.querySelectorAll('.ForumPostUserArea')).forEach(function (td) {
+		td.rowSpan = 2;
+	});
+	document.head.appendChild(document.createElement('style')).textContent = '
+	.ForumPostFooterArea, .ForumPostSignature {
+		border-top: 1px dotted; font-size: smaller;
+	}
+	.ForumPostFooterArea li:empty {
+		display: none;
+	}
+	.ForumPostUserContent {
+		padding-right: 1em;
+	}
+	:visited, .ForumLastPost a {
+		opacity: 0.25;
+	}';
+})();
