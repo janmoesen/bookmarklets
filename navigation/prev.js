42ee62b0543956c59d9c36d66cc9488dfd097d80
Jan Moesen <github.com@moesen.nu>
2011-10-01 23:20:25 +0200

    « Previous: also search for #previousBla
    (Instead of just #prevBla.)

--- navigation/prev.js
+++ navigation/prev.js
@@ -17,7 +17,7 @@
 	var symbols = '<< « ← ⇐',
 	    keywords = ('Previous previous Older older Vorige vorige Ouder ouder Précédent précédent ' + symbols).split(' '),
 	    symbols = symbols.split(' '),
-	    identifiers = 'prev prevArticle prevPost prevLink'.split(' '),
+	    identifiers = 'prev previous prevArticle previousArticle prevPost previousPost prevLink previousLink'.split(' '),
 	    monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
 	    selectors, newUrl;
 
