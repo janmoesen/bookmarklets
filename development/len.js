616004f501a8944d2b8ecd27e5ed0198de5610bc
Jan Moesen <github.com@moesen.nu>
2011-11-19 00:44:39 +0100

    Show length: copy BestieJS String.length fix

--- development/len.js
+++ development/len.js
@@ -6,22 +6,38 @@
 (function len() {
 	var s = (<><![CDATA[%s]]></> + '').replace(/\u0025s/, '') || getSelection() + '' || prompt('String?');
 	if (s) {
-		var maxDisplayLength = 64;
-		var displayString = s.length > maxDisplayLength
-			? s.substring(0, maxDisplayLength / 2) + '…' + s.substring(s.length - maxDisplayLength / 2 + 1)
-			: s;
-
-		/* To count the number of bytes, we replace all ASCII characters by a
-		 * URL-safe "a". If we URL-encode the resulting string, we get all
-		 * non-ASCII characters encoded as sequences of %XX. We then replace
-		 * those triplets by a single character, "b", and count the
-		 * characters in the "aaabbbaaaa" string.
+		/* To count the number of characters, we cannot rely on String.length
+		 * because of astral chars. Use this code from BestieJS to deal with
+		 * those:
+		 * https://github.com/bestiejs/punycode.js/blob/8164242ef1/punycode.js#L99-126
 		 */
-		var numBytes = encodeURIComponent(s.replace(/[\x00-\x7F]/g, 'a')).replace(/%[0-9A-Fa-f]{2}/g, 'b').length;
+		var realLength = 0, counter = 0, length = s.length, value, extra;
+		while (counter < length) {
+			value = s.charCodeAt(counter++);
+			if ((value & 0xF800) == 0xD800) {
+				extra = s.charCodeAt(counter++);
+				if ((value & 0xFC00) != 0xD800 || (extra & 0xFC00) != 0xDC00) {
+					alert('Illegal UTF-16 sequence, but continuing anyway.');
+				}
+				value = ((value & 0x3FF) << 10) + (extra & 0x3FF) + 0x10000;
+			}
+			realLength++;
+		}
 
-		displayString = s.length === numBytes
+		/* To count the number of bytes, we URL-encode the string so the
+		 * non-ASCII characters are encoded as sequences of %XX. We then
+		 * replace those triplets by a single character, "x", and count the
+		 * number of characters in the resulting string.
+		 */
+		var numBytes = encodeURI(s).replace(/%../g, 'x').length;
+
+		var maxDisplayLength = 64;
+		var displayString = realLength > maxDisplayLength
+			? s.substring(0, maxDisplayLength / 2) + '…' + s.substring(realLength - maxDisplayLength / 2 + 1)
+			: s;
+		displayString = realLength === numBytes
 			? 'The number of characters in the ASCII string "' + displayString + '" is: '
 			: 'The number of characters in the non-ASCII string "' + displayString + '" (' + numBytes + ' bytes) is: ';
-		prompt(displayString, s.length);
+		prompt(displayString, realLength);
 	}
 })();
