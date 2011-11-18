b2da46f40c042f650c8f911a649684e672d8a4f2
Jan Moesen <github.com@moesen.nu>
2011-11-18 23:43:48 +0100

    Show length: show byte count for non-ASCII strings
    In response to https://twitter.com/#!/mathias/status/137593750829543424
    and https://twitter.com/#!/LeaVerou/status/137644776915869696

--- development/len.js
+++ development/len.js
@@ -4,9 +4,23 @@
  * @title Show length
  */
 (function len() {
-	var maxLength = 64;
 	var s = (<><![CDATA[%s]]></> + '').replace(/\u0025s/, '') || getSelection() + '' || prompt('String?');
 	if (s) {
-		prompt('The length of "' + (s.length > maxLength ? s.substring(0, maxLength / 2) + '…' + s.substring(s.length - maxLength / 2 + 1) : s) + '" is: ', s.length);
+		var maxDisplayLength = 64;
+		var displayString = s.length > maxDisplayLength
+			? s.substring(0, maxDisplayLength / 2) + '…' + s.substring(s.length - maxDisplayLength / 2 + 1)
+			: s;
+		/* To count the number of bytes, we replace all ASCII characters by a
+		 * URL-safe "a". If we URL-encode the resulting string, we get all
+		 * non-ASCII characters encoded as sequences of %XX. We then replace
+		 * those triplets by a single character, "b", and count the
+		 * characters in the "aaabbbaaaa" string.
+		 */
+		var numBytes = encodeURIComponent(s.replace(/[\x00-\x7F]/g, 'a')).replace(/%[0-9A-Fa-f]{2}/g, 'b').length;
+
+		displayString = s.length === numBytes
+			? 'The number of characters in the ASCII string "' + displayString + '" is: '
+			: 'The number of characters in the non-ASCII string "' + displayString + '" (' + numBytes + ' bytes) is: ';
+		prompt(displayString, s.length);
 	}
 })();
