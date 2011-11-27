ee08baffda141e76c11d2f5aaf51912fa182eccd
Jan Moesen <github.com@moesen.nu>
2011-11-27 14:58:27 +0100

    Translate to *: improve URL parameter detection

--- language/translations/2it.js
+++ language/translations/2it.js
@@ -7,7 +7,7 @@
 (function () {
 	var s = (<><![CDATA[%s]]></> + '').replace(/\u0025s/, '') || getSelection() + '' || (location.protocol === 'file:' ? '' : location + '') || prompt('Please enter your text:');
 	if (s) {
-		location = s.match(/^(\w+:(\/\/)?)?[^\s]+(\.[^\s])+/)
+		location = s.match(/^(\w+:(\/\/)?)?[^\s.]+(\.[^\s])+/)
 			? 'http://translate.google.com/translate?sl=auto&tl=it&u=' + encodeURIComponent(s)
 			: 'http://translate.google.com/translate_t#auto|it|' + encodeURIComponent(s);
 	}
