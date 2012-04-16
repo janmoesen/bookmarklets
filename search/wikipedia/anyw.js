8cccdaa53fef3a00113bc9ce14ee3d69ad30b7ef
Jan Moesen <github.com@moesen.nu>
2012-04-16 20:25:46 +0200

    Any Wikipedia: actually restrict to wikipedia.org
    "D'", as they say, "oh!"

--- search/wikipedia/anyw.js
+++ search/wikipedia/anyw.js
@@ -9,6 +9,6 @@
 	var s = (<><![CDATA[%s]]></> + '').replace(/\u0025s/, '') || getSelection() + '' || prompt('Please enter your text:');
 	if (s) {
 		/* Pro-tip: use this as keyword.URL in Firefox (see about:config). It used to be the default, but then Google slightly tweaked it. */
-		location = 'http://www.google.com/search?btnI=&ie=utf-8&sourceid=navclient&q=' + encodeURIComponent(s);
+		location = 'http://www.google.com/search?btnI=&ie=utf-8&sourceid=navclient&q=' + encodeURIComponent('site:wikipedia.org ' + s);
 	}
 })();
