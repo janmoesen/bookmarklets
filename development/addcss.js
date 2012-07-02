51380bd98fe81c2e789789c563660deb6d1c98a7
Jan Moesen <github.com@moesen.nu>
2012-07-02 15:35:47 +0200

    Add CSS: recurse for frames and iframes

--- development/addcss.js
+++ development/addcss.js
@@ -3,9 +3,18 @@
  *
  * @title Add CSS
  */
-(function addcss() {
+(function addcss(document) {
 	var s = (<><![CDATA[%s]]></> + '').replace(/\u0025s/, '') || getSelection() + '' || prompt('Please enter your CSS code:');
 	if (s) {
 		document.head.appendChild(document.createElement('style')).textContent = s;
 	}
-})();
+
+	/* Recurse for frames and iframes. */
+	try {
+		Array.prototype.slice.call(document.querySelectorAll('frame, iframe, object[type^="text/html"], object[type^="application/xhtml+xml"]')).forEach(function (elem) {
+			addcss(elem.contentDocument);
+		});
+	} catch (e) {
+		/* Catch exceptions for out-of-domain access, but do not do anything with them. */
+	}
+})(document);
