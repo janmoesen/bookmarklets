71d4e4606b413113d9f82b7b8d4c41676a316aea
Jan Moesen <github.com@moesen.nu>
2012-07-25 07:59:14 +0200

    YouTube: go to video ID, if given

--- search/yt.js
+++ search/yt.js
@@ -1,11 +1,17 @@
 /**
  * Search YouTube for the specified or selected text.
  *
+ * If the text looks like a video ID, you are taken directly to the video.
+ *
  * @title YouTube
  */
 (function yt() {
 	var s = (<><![CDATA[%s]]></> + '').replace(/\u0025s/, '') || getSelection() + '' || prompt('Please enter your text:');
 	if (s) {
-		location = 'http://www.youtube.com/results?search_query=' + encodeURIComponent(s);
+		if (s.match(/^[-_a-zA-Z0-9]{11}$/) && s.match(/[A-Z][a-z]|[a-z][A-Z]/)) {
+			location = 'http://www.youtube.com/watch?v=' + encodeURIComponent(s);
+		} else {
+			location = 'http://www.youtube.com/results?search_query=' + encodeURIComponent(s);
+		}
 	}
 })();
