588b987bad78801fce7f54d181a09043973f60a5
Jan Moesen <github.com@moesen.nu>
2012-08-23 22:11:04 +0200

    YouTube: improve video ID checking
    If the search string is 11 characters, chances are pretty high already
    that we are dealing with a video ID. If there is a letter followed by
    a number, or vice versa, it is almost certainly a video ID.
    
    Test case: yt -3fa-sjsvno

--- search/yt.js
+++ search/yt.js
@@ -8,7 +8,7 @@
 (function yt() {
 	var s = (<><![CDATA[%s]]></> + '').replace(/\u0025s/, '') || getSelection() + '' || prompt('Please enter your text:');
 	if (s) {
-		if (s.match(/^[-_a-zA-Z0-9]{11}$/) && s.match(/[A-Z][a-z]|[a-z][A-Z]/)) {
+		if (s.match(/^[-_a-zA-Z0-9]{11}$/) && s.match(/[A-Z][a-z]|[a-z][A-Z]|[0-9][a-zA-Z]|[a-zA-Z][0-9]/)) {
 			location = 'http://www.youtube.com/watch?v=' + encodeURIComponent(s);
 		} else {
 			location = 'http://www.youtube.com/results?search_query=' + encodeURIComponent(s);
