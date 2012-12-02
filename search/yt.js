93afe320bf7d22f26f9c9a4424cfd798e86cf4cd
Jan Moesen <github.com@moesen.nu>
2012-12-02 15:02:53 +0100

    YouTube: remove BOM from text
    YouTube wedges it in between characters inside comments, which makes it annoying when
    copy/pasting video IDs, for example.

--- search/yt.js
+++ search/yt.js
@@ -11,6 +11,7 @@
 (function yt() {
 	var s = (<><![CDATA[%s]]></> + '').replace(/\u0025s/, '') || getSelection() + '' || prompt('Please enter your text:');
 	if (s) {
+		s = s.replace(/\uFEFF/g, '');
 		var matches;
 		if ((matches = s.match(/^([-_a-zA-Z0-9]{11})( *!)?$/)) && s.match(/[A-Z][a-z]|[a-z][A-Z]|[0-9][a-zA-Z]|[a-zA-Z][0-9]/)) {
 			if (matches[2]) {
