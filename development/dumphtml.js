44646d8e2799765e8ca835713c29436a4dc3af4d
Jan Moesen <github.com@moesen.nu>
2013-02-03 11:16:43 +0100

    Dump HTML: initial commit (dump generated DOM)

---
+++ development/dumphtml.js
@@ -0,0 +1,127 @@
+/**
+ * Dump the generated HTML of the current page as a view-source: document in a
+ * new tab or window.
+ *
+ * @title Dump HTML
+ */
+(function dumphtml() {
+	/* Use different serializers for different nodes. */
+	var stringifiers = {};
+	stringifiers[Node.ELEMENT_NODE] = function handleElementNode(node) {
+		return node.outerHTML;
+	};
+
+	stringifiers[Node.ATTRIBUTE_NODE] = function handleAttributeNode(node) {
+		if (typeof console !== 'undefined' && console.debug) {
+			console.debug('dump: unexpected ATTRIBUTE_NODE');
+		}
+
+		return '<!-- Unhandled ATTRIBUTE_NODE -->';
+	};
+
+	stringifiers[Node.TEXT_NODE] = function handleTextNode(node) {
+		var tempDiv = document.createElement('div');
+		tempDiv.textContent = node.textContent;
+
+		return tempDiv.innerHTML;
+	};
+
+	stringifiers[Node.CDATA_SECTION_NODE] = function handleCdataSectionNode(node) {
+		return '<![CDATA[' + node.textContent + ']]>';
+	};
+
+	stringifiers[Node.ENTITY_REFERENCE_NODE] = function handleEntityReferenceNode(node) {
+		if (typeof console !== 'undefined' && console.debug) {
+			console.debug('dump: unexpected ENTITY_REFERENCE_NODE');
+		}
+
+		return '<!-- Unhandled ENTITY_REFERENCE_NODE -->';
+	};
+
+	stringifiers[Node.ENTITY_NODE] = function handleEntityNode(node) {
+		if (typeof console !== 'undefined' && console.debug) {
+			console.debug('dump: unexpected ENTITY_NODE');
+		}
+
+		return '<!-- Unhandled ENTITY_NODE -->';
+	};
+
+	stringifiers[Node.PROCESSING_INSTRUCTION_NODE] = function handleProcessingInstructionNode(node) {
+		if (typeof console !== 'undefined' && console.debug) {
+			console.debug('dump: unexpected PROCESSING_INSTRUCTION_NODE');
+		}
+
+		return '<!-- Unhandled PROCESSING_INSTRUCTION_NODE -->';
+	};
+
+	stringifiers[Node.COMMENT_NODE] = function handleCommentNode(node) {
+		return '<!--' + node.data + '-->';
+	};
+
+	stringifiers[Node.DOCUMENT_NODE] = function handleDocumentNode(node) {
+		if (typeof console !== 'undefined' && console.debug) {
+			console.debug('dump: unexpected DOCUMENT_NODE');
+		}
+
+		return '<!-- Unhandled DOCUMENT_NODE -->';
+	};
+
+	stringifiers[Node.DOCUMENT_TYPE_NODE] = function handleDocumentTypeNode(node) {
+		var docTypeParts = ['DOCTYPE', node.name];
+
+		if (node.publicId) {
+			docTypeParts.push('PUBLIC', node.publicId);
+		}
+
+		if (node.systemId) {
+			docTypeParts.push(node.systemId);
+		};
+	
+		return '<!' + docTypeParts.join(' ') + '>';
+	};
+
+	stringifiers[Node.DOCUMENT_FRAGMENT_NODE] = function handleDocumentFragmentNode(node) {
+		if (typeof console !== 'undefined' && console.debug) {
+			console.debug('dump: unexpected DOCUMENT_FRAGMENT_NODE');
+		}
+
+		return '<!-- Unhandled DOCUMENT_FRAGMENT_NODE -->';
+	};
+
+	stringifiers[Node.NOTATION_NODE] = function handleNotationNode(node) {
+		if (typeof console !== 'undefined' && console.debug) {
+			console.debug('dump: unexpected NOTATION_NODE');
+		}
+
+		return '<!-- Unhandled NOTATION_NODE -->';
+	};
+
+	/* Use an array + join to concatenate all HTML string parts. */
+	var htmlParts = [];
+
+	/* Ensure there is a <base href> so relative links keep working. */
+	/* TODO: add a check for in-page links (e.g. href="#foo"). Might need
+	 * reworking this logic to rewrite all links except those in-page ones,
+	 * because the <base href> overrides everything. */
+	var baseHref;
+	if (document.documentElement.nodeName.toUpperCase() === 'HTML' && !document.querySelector('base[href]')) {
+		baseHref = document.createElement('base');
+		baseHref.href = document.location;
+		document.head.appendChild(baseHref);
+	}
+
+	/* Go! */
+	Array.prototype.slice.call(document.childNodes).forEach(function (node) {
+		if (stringifiers[node.nodeType]) {
+			htmlParts.push(stringifiers[node.nodeType](node));
+		}
+	});
+
+	/* Remove any <base href> added we added. */
+	if (baseHref) {
+		baseHref.parentNode.removeChild(baseHref);
+	}
+
+	/* Show the result in a new tab or window. */
+	window.open('view-source:data:text/html;charset=UTF-8,' + encodeURIComponent(htmlParts.join('\n')));
+})();
