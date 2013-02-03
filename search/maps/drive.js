32dbb92e6c997a6661bb860239c3fbaefbc8cb65
Jan Moesen <github.com@moesen.nu>
2013-02-03 15:04:54 +0100

    Google Maps: initial commit (map/drive/ride/walk)

---
+++ search/maps/drive.js
@@ -0,0 +1,27 @@
+/**
+ * Search Google Maps for a route by car.
+ *
+ * @title Google Maps (for cars)
+ */
+(function drive() {
+	/* Try to get the parameter string from the bookmarklet/search query. */
+	/* Fall back to the current text selection, if any. If those options
+	/* both fail, prompt the user. */
+	var s = (function () { /*%s*/ }).toString()
+		.replace(/^function\s*\(\s*\)\s*\{\s*\/\*/, '')
+		.replace(/\*\/\s*\;?\s*\}\s*$/, '')
+		.replace(/\u0025s/, '');
+	if (s === '') {
+		s = getSelection() + '' || prompt('Please enter your URL:', location);
+	}
+
+	if (s) {
+		if (s.match(/\sto[\s:]/) && !s.match(/^\s*from[\s:]/)) {
+			s = 'from:' + s;
+		}
+
+		s = s.replace(/^\s*from\s+/g, 'from:');
+		s = s.replace(/\s+to\s+/g, ' to:');
+		location = 'https://maps.google.com/?dirflg=c&q=' + encodeURIComponent(s);
+	}
+})();
