f788eacef063c51d7b2b2c37e9c8028ee5cffaff
Jan Moesen <github.com@moesen.nu>
2013-08-05 16:15:32 +0200

    Set title: initial commit

---
+++ development/title.js
@@ -0,0 +1,22 @@
+/**
+ * Set the document title to the specified text.
+ *
+ * @title Set title
+ */
+(function title() {
+	/* Try to get the parameter string from the bookmarklet/search query.
+	   Fall back to the current text selection, if any. If those options
+	   both fail, prompt the user.
+	*/
+	var s = (function () { /*%s*/; }).toString()
+		.replace(/^function\s*\(\s*\)\s*\{\s*\/\*/, '')
+		.replace(/\*\/\s*\;?\s*\}\s*$/, '')
+		.replace(/\u0025s/, '');
+	if (s === '') {
+		s = getSelection() + '' || prompt('Please enter the title:', document.title);
+	}
+
+	if (s !== null) {
+		document.title = s;
+	}
+})();
