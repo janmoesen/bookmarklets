42e170a5cb79e8526384c8ec382dec137791c14f
Jan Moesen <github.com@moesen.nu>
2013-08-05 16:17:42 +0200

    Google Site Search: initial commit

---
+++ search/gs.js
@@ -0,0 +1,24 @@
+/**
+ * Search the international (English) Google for the given text on the current
+ * site's domain, with a 100 results per SERP and without Instant Search.
+ *
+ * @title Google Site Search
+ */
+(function gs() {
+	/* Try to get the parameter string from the bookmarklet/search query.
+	   Fall back to the current text selection, if any. If those options
+	   both fail, prompt the user.
+	*/
+	var s = (function () { /*%s*/; }).toString()
+		.replace(/^function\s*\(\s*\)\s*\{\s*\/\*/, '')
+		.replace(/\*\/\s*\;?\s*\}\s*$/, '')
+		.replace(/\u0025s/, '');
+	if (s === '') {
+		s = getSelection() + '' || prompt('Please enter your text:');
+	}
+
+	if (s) {
+		s = 'site:' + location.host + ' ' + s;
+		location = 'https://www.google.com/search?as_qdr=all&ie=utf-8&hl=en&num=100&q=' + encodeURIComponent(s);
+	}
+})();
