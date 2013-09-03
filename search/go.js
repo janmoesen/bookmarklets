1c8a3d8d80408dbeb93892e3ab6b67dcda0dcac2
Jan Moesen <github.com@moesen.nu>
2013-09-03 08:45:13 +0200

    Go: use DuckDuckGo instead of Google
    Google's "I'm Feeling Lucky" was working less reliably, often just
    showing the normal SERPs instead of redirecting to the top result.

--- search/go.js
+++ search/go.js
@@ -1,9 +1,5 @@
 /**
- * Go to the first Google result ("I'm Feeling Lucky") for the given text.
- *
- * This used to be the default for "keyword.URL" in Firefox, but looks like it
- * is going to be removed: "738818 – consolidate Firefox search preferences":
- * https://bugzilla.mozilla.org/show_bug.cgi?id=738818
+ * Go to the first DuckDuckGo result ("I'm Feeling Ducky") for the given text.
  *
  * @title Go
  */
@@ -21,6 +17,7 @@
 	}
 
 	if (s) {
-		location = 'https://www.google.com/search?btnI&ie=utf-8&sourceid=navclient&q=' + encodeURIComponent(s);
+		s += ' !';
+		location = 'https://www.duckduckgo.com/?q=' + encodeURIComponent(s);
 	}
 })();
