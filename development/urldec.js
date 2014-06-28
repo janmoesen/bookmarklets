fc5e8e7b2e7369fe4c1e604bb6c3565b22f2689c
Jan Moesen <github.com@moesen.nu>
2014-06-28 14:25:02 +0200

    URL-decode: initial commit
    Use a data: URL to decode all %XX into UTF-8. (Which is more
    fault-tolerant than URIDecode*().

---
+++ development/urldec.js
@@ -0,0 +1,24 @@
+/**
+ * Decode all %XX bytes into a (hopefully) readable string.
+ *
+ * @title URL-decode
+ */
+(function urldec() {
+	/* Try to get the parameter string from the bookmarklet/search query.
+	   Fall back to the current text selection, if any. If those options
+	   both fail, prompt the user.
+	*/
+	var s = (function () { /*%s*/; }).toString()
+		.replace(/^function\s*\(\s*\)\s*\{\s*\/\*/, '')
+		.replace(/\*\/\s*\;?\s*\}\s*$/, '')
+		.replace(/\u0025s/, '');
+	if (s === '') {
+		s = getSelection() + '' || prompt('Please enter your text:');
+	}
+
+	if (!location || location == 'about:blank') {
+		location = 'data:text/plain;charset=UTF-8,' + s;
+	} else {
+		window.open('data:text/plain;charset=UTF-8,' + s);
+	}
+})();
