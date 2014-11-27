05d525a2513ab4d3730fc6ec123e8c41793fde44
Jan Moesen <github.com@moesen.nu>
2014-11-28 00:09:17 +0100

    Wiktionary: add IT and ES

---
+++ language/dictionaries/eswikt.js
@@ -0,0 +1,22 @@
+/**
+ * Look up the specified or selected text using Spanish Wiktionary.
+ *
+ * @title Spanish Wiktionary
+ */
+(function eswikt() {
+	/* Try to get the parameter string from the bookmarklet/search query.
+	   Fall back to the current text selection, if any. If those options
+	   both fail, prompt the user.
+	*/
+	var s = (function () { /*%s*/; }).toString()
+		.replace(/^function\s*\(\s*\)\s*\{\s*\/\*/, '')
+		.replace(/\*\/\s*\;?\s*\}\s*$/, '')
+		.replace(/\u0025s/, '');
+	if (s === '') {
+		s = getSelection() + '' || prompt('Please enter your text:');
+	}
+
+	if (s) {
+		location = 'https://es.wiktionary.org/wiki/' + encodeURIComponent(s);
+	}
+})();
---
+++ language/dictionaries/itwikt.js
@@ -0,0 +1,22 @@
+/**
+ * Look up the specified or selected text using Italian Wiktionary.
+ *
+ * @title Italian Wiktionary
+ */
+(function itwikt() {
+	/* Try to get the parameter string from the bookmarklet/search query.
+	   Fall back to the current text selection, if any. If those options
+	   both fail, prompt the user.
+	*/
+	var s = (function () { /*%s*/; }).toString()
+		.replace(/^function\s*\(\s*\)\s*\{\s*\/\*/, '')
+		.replace(/\*\/\s*\;?\s*\}\s*$/, '')
+		.replace(/\u0025s/, '');
+	if (s === '') {
+		s = getSelection() + '' || prompt('Please enter your text:');
+	}
+
+	if (s) {
+		location = 'https://it.wiktionary.org/wiki/' + encodeURIComponent(s);
+	}
+})();
