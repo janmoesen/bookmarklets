7ae2d78242b89f52fabf96713fed78287cfd2d8c
Jan Moesen <github.com@moesen.nu>
2016-01-09 00:29:02 +0100

    Kill overlays: initial commit

---
+++ content/killoverlays.js
@@ -0,0 +1,38 @@
+/**
+ * Get rid of common overlays, such as full-page ads.
+ *
+ * @title Kill overlays
+ */
+(function killoverlays() {
+	/* Remove "suspicious" elements. */
+	var selectors = [
+		'body [class*="modal"]',
+		'body [class*="overlay"]',
+		'body [class*="popup"]',
+		'body [style*="width"][style*="height"][style*="100%"]',
+		'.frame-container',
+		'.optimonk-holder',
+		'iframe[style]',
+		'#spout-friendly-iframe',
+	];
+
+	[].forEach.call(
+		document.querySelectorAll(selectors.join(', ')),
+		function (elem) {
+			if (elem.offsetWidth / window.innerWidth > 0.75 || elem.offsetHeight / window.innerHeight > 0.75) {
+				elem.parentNode.removeChild(elem);
+			}
+		}
+	);
+
+	/* Re-enable scrolling on Quora.com. */
+	document.body.classList.remove('login_no_scroll');
+
+	/* Re-enable scrolling disabled by inline styles. */
+	[].forEach.call(
+		document.querySelectorAll('[style*="overflow"][style*="hidden"]'),
+		function (elem) {
+			elem.setAttribute('style', elem.getAttribute('style').replace(/overflow\s*:\s*hidden\s*;?/, ''));
+		}
+	);
+})();
