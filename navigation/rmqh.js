ebf0ee5f86df780aebc5bddd5081d9793fb82310
Jan Moesen <github.com@moesen.nu>
2016-01-14 15:54:36 +0100

    Remove query/hash/*: support --no-reload
    Change the URL in the location bar, but do not actually navigate/reload.

--- navigation/rmqh.js
+++ navigation/rmqh.js
@@ -10,6 +10,15 @@
 	var newLocation = oldLocation.replace(/[#?].*/, '');
 
 	if (newLocation !== oldLocation) {
-		location = newLocation;
+		/* See if the user wants to replace the URL without reloading. */
+		var s = (function () { /*%s*/; }).toString()
+			.replace(/^function\s*\(\s*\)\s*\{\s*\/\*/, '')
+			.replace(/\*\/\s*\;?\s*\}\s*$/, '')
+			.replace(/\u0025s/, '');
+		if ((' ' + s + ' ').match(/ --(no|skip)-reload /)) {
+			history.pushState({}, document.title, newLocation);
+		} else {
+			location = newLocation;
+		}
 	}
 })();
