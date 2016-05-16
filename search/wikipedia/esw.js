3420ef42910d76c08f98aab6e0d400413a8ad8ce
Jan Moesen <github.com@moesen.nu>
2016-05-16 10:27:39 +0200

    Wikipedia *: add missing Spanish

---
+++ search/wikipedia/esw.js
@@ -0,0 +1,25 @@
+/**
+ * Look up the specified or selected text in the Spanish Wikipedia.
+ *
+ * @title Spanish Wikipedia
+ */
+(function esw() {
+	/* Try to get the parameter string from the bookmarklet/search query.
+	   Fall back to the current text selection, if any. If those options
+	   both fail, prompt the user.
+	*/
+	var s = (function () { /*%s*/; }).toString()
+		.replace(/^function\s*\(\s*\)\s*\{\s*\/\*/, '')
+		.replace(/\*\/\s*\;?\s*\}\s*$/, '')
+		.replace(/\u0025s/, '');
+	if (s === '') {
+		s = getSelection() + '' || prompt('Please enter your text:');
+	} else {
+		s = s.replace(/(^|\s|")~("|\s|$)/g, '$1' + getSelection() + '$2');
+	}
+
+	if (s) {
+		/* The Wikipedia search works like "I'm feeling lucky" on most Wikipedia instances. If there is a complete match, it will redirect us there. */
+		location = 'https://es.wikipedia.org/w/index.php?searchToken=&title=Special%3ASearch&search=' + encodeURIComponent(s);
+	}
+})();
