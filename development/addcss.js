94c2a1c25421ba48086a29aa634126c233491ec3
Jan Moesen <github.com@moesen.nu>
2017-07-25 23:47:57 +0200

    addcss: only check for parameter once

--- development/addcss.js
+++ development/addcss.js
@@ -3,12 +3,12 @@
  *
  * @title Add CSS
  */
-(function addcss(document) {
+(function addcss(document, s) {
 	/* Try to get the parameter string from the bookmarklet/search query.
 	   Fall back to the current text selection, if any. If those options
 	   both fail, prompt the user.
 	*/
-	var s = (function () { /*%s*/; }).toString()
+	s = s || (function () { /*%s*/; }).toString()
 		.replace(/^function\s*\(\s*\)\s*\{\s*\/\*/, '')
 		.replace(/\*\/\s*\;?\s*\}\s*$/, '')
 		.replace(/\u0025s/, '');
@@ -20,14 +20,14 @@
 
 	if (s) {
 		document.head.appendChild(document.createElement('style')).textContent = s;
-	}
 
-	/* Recurse for frames and iframes. */
-	try {
-		Array.prototype.slice.call(document.querySelectorAll('frame, iframe, object[type^="text/html"], object[type^="application/xhtml+xml"]')).forEach(function (elem) {
-			addcss(elem.contentDocument);
-		});
-	} catch (e) {
-		/* Catch exceptions for out-of-domain access, but do not do anything with them. */
+		/* Recurse for frames and iframes. */
+		try {
+			Array.prototype.slice.call(document.querySelectorAll('frame, iframe, object[type^="text/html"], object[type^="application/xhtml+xml"]')).forEach(function (elem) {
+				addcss(elem.contentDocument, s);
+			});
+		} catch (e) {
+			/* Catch exceptions for out-of-domain access, but do not do anything with them. */
+		}
 	}
 })(document);
