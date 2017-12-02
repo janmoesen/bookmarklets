c90891352f9263eaba8e64525040670da9e524ca
Jan Moesen <github.com@moesen.nu>
2017-12-02 22:03:11 +0100

    shuo: initial commit (speak Mandarin Chinese)
    Thanks, Naver! :-) (Although it fails as soon as there is one character that is
    not Chinese text. Bummer.)

---
+++ language/shuo.js
@@ -0,0 +1,69 @@
+/**
+ * Use Naver’s text-to-speech on the selected (Mandarin Chinese) text.
+ *
+ * @title Shuō (说)
+ */
+(function shuo() {
+	/* Try to get the parameter string from the bookmarklet/search query.
+	   Fall back to the current text selection, if any. If those options
+	   both fail, prompt the user.
+	*/
+	var s = (function () { /*%s*/; }).toString()
+		.replace(/^function\s*\(\s*\)\s*\{\s*\/\*/, '')
+		.replace(/\*\/\s*\;?\s*\}\s*$/, '')
+		.replace(/\u0025s/, '');
+
+	/**
+	 * Get the active text selection, diving into frames and
+	 * text controls when necessary and possible.
+	 */
+	function getActiveSelection(doc) {
+		if (arguments.length === 0) {
+			doc = document;
+		}
+
+		if (!doc || typeof doc.getSelection !== 'function') {
+			return '';
+		}
+
+		if (!doc.activeElement) {
+			return doc.getSelection() + '';
+		}
+
+		var activeElement = doc.activeElement;
+
+		/* Recurse for FRAMEs and IFRAMEs. */
+		try {
+			if (
+				typeof activeElement.contentDocument === 'object'
+				&& activeElement.contentDocument !== null
+			) {
+				return getActiveSelection(activeElement.contentDocument);
+			}
+		} catch (e) {
+			return doc.getSelection() + '';
+		}
+
+		/* Get the selection from inside a text control. */
+		if (
+			typeof activeElement.value === 'string'
+			&& activeElement.selectionStart !== activeElement.selectionEnd
+		) {
+			return activeElement.value.substring(activeElement.selectionStart, activeElement.selectionEnd);
+		}
+
+		/* Get the normal selection. */
+		return doc.getSelection() + '';
+	}
+
+	if (s === '') {
+		s = getActiveSelection() || prompt('Please enter the text to speak:');
+	} else {
+		s = s.replace(/(^|\s|")~("|\s|$)/g, '$1' + getActiveSelection() + '$2');
+	}
+
+	if (s) {
+		var mp3Url = 'http://tts.cndic.naver.com/tts/mp3ttsV1.cgi?spk_id=250&text_fmt=0&pitch=100&volume=200&speed=80&wrapper=0&enc=0&text=' + encodeURI(s);
+		new Audio(mp3Url).play().catch(error => alert('Could not play Naver’s text-to-speech. This can have many causes, including ad and tracker blockers.\n\nError: ' + error + '\n\nOriginal text:\n' + s));
+	}
+})();
