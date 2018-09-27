48da918eb534910774ce02f86125f5787f32900a
Jan Moesen <github.com@moesen.nu>
2018-09-27 22:50:35 +0200

    somanp: initial commit
    Show the currently playing track in the window/tab title on SomaFM Player pages,
    e.g. http://somafm.com/player/#/now-playing/cliqhop

---
+++ site-specific/somanp.js
@@ -0,0 +1,36 @@
+/**
+ * Show the currently playing track in the window/tab title on SomaFM Player
+ * pages like http://somafm.com/player/#/now-playing/cliqhop
+ *
+ * @title SomaFM np
+ */
+(function somanp() {
+	var playlistContainer = document.querySelector('.now-playing .list-body');
+	if (!playlistContainer) {
+		return;
+	}
+
+	var originalTitle = document.title;
+
+	function updateTitle() {
+		var artistNode = playlistContainer.querySelector('.row > :nth-child(2)');
+		var titleNode = playlistContainer.querySelector('.row > :nth-child(3)');
+
+		if (!artistNode || !titleNode) {
+			return;
+		}
+
+		document.title = artistNode.textContent
+			+ ' - ' + titleNode.textContent.toLowerCase()
+			+ ' | ' + originalTitle;
+	}
+
+	var observer = new MutationObserver(updateTitle);
+
+	observer.observe(playlistContainer, {
+		childList: true,
+		subtree: true
+	});
+
+	updateTitle();
+})();
