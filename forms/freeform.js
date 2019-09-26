fd9960166b5de852a37808f13e31a49f242b4c4c
Jan Moesen <github.com@moesen.nu>
2019-09-26 09:40:31 +0200

    freeform: also process elements outside of FORMs
    The diff only looks big because the indent level has decreased. Use “-w”.

--- forms/freeform.js
+++ forms/freeform.js
@@ -34,117 +34,115 @@
 		'button',
 	];
 
-	[].forEach.call(document.forms, function (form) {
-		[].forEach.call(form.elements, function (element) {
-			var tooltipLines = [];
+	[].forEach.call(document.querySelectorAll('input, textarea, select'), function (element) {
+		var tooltipLines = [];
 
-			/* Put the original attribute values in the tooltip. */
-			Object.keys(originalAttributesToPutInTooltip).forEach(function (attrName) {
-				if (element.hasAttribute(attrName) || element[attrName]) {
-					var attrValue = element.getAttribute(attrName) || element[attrName];
-					if (attrValue !== null && attrValue !== '') {
-						tooltipLines.push(originalAttributesToPutInTooltip[attrName] + ': “' + attrValue + '”');
-					}
+		/* Put the original attribute values in the tooltip. */
+		Object.keys(originalAttributesToPutInTooltip).forEach(function (attrName) {
+			if (element.hasAttribute(attrName) || element[attrName]) {
+				var attrValue = element.getAttribute(attrName) || element[attrName];
+				if (attrValue !== null && attrValue !== '') {
+					tooltipLines.push(originalAttributesToPutInTooltip[attrName] + ': “' + attrValue + '”');
 				}
-			});
-
-			/* Remove unwanted attributes. */
-			attributeNamesToRemove.forEach(function (attrName) {
-				if (element.hasAttribute(attrName) || element[attrName]) {
-					console.log('Freeform: remove “' + attrName + '” attribute on element: ', element);
-					tooltipLines.push('Removed “' + attrName + '” attribute; was: “' + (element.getAttribute(attrName) || element[attrName]) + '”');
-
-					delete element[attrName];
-					element.removeAttribute(attrName);
-				}
-			});
-
-			if (element.tagName.toUpperCase() === 'INPUT') {
-				if (element.hasAttribute('type')) {
-					var type = element.getAttribute('type').toLowerCase();
-					/* Change unwanted INPUT types to default INPUTs. */
-					if (type !== '' && allowedInputTypes.indexOf(type) === -1) {
-						console.log('Freeform: remove “type” attribute on element: ', element);
-						tooltipLines.push('Removed “type” attribute; was: “' + element.getAttribute('type') + '”');
-
-						element.removeAttribute('type');
-					}
-
-					/* Remove MIME type restriction on file uploads. */
-					if (type === 'file') {
-						element.removeAttribute('accept');
-					}
-				}
-
-				/* Change all default (text) INPUTs to multiline TEXTAREAs. */
-				if (!element.hasAttribute('type')) {
-					var textarea = element.ownerDocument.createElementNS('http://www.w3.org/1999/xhtml', 'textarea');
-
-					textarea.name = element.name;
-					textarea.value = element.value;
-					textarea.rows = 1;
-
-					if (element.placeholder) {
-						textarea.placeholder = element.placeholder;
-					}
-
-					if (element.id) {
-						textarea.id = element.id;
-					}
-
-					element.parentNode.replaceChild(textarea, element);
-					element = textarea;
-
-					var computedStyle = getComputedStyle(textarea);
-					textarea.style.height = textarea.style.minHeight =
-						parseInt(computedStyle.paddingTop || 0, 10)
-						+ parseInt(computedStyle.lineHeight || 0, 10)
-						+ parseInt(computedStyle.paddingBottom || 0, 10)
-						+ 'px';
-
-					tooltipLines.push('Changed from INPUT to TEXTAREA');
-				}
-			} else if (element.tagName.toUpperCase() === 'SELECT') {
-				/* Make sure there is at least one option to change. */
-				element.insertBefore(document.createElementNS('http://www.w3.org/1999/xhtml', 'option'), element.firstChild);
-
-				/* Put the original OPTION value in the tooltip. */
-				[].forEach.call(element.options, function (option) {
-					if (option.hasAttribute('value')) {
-						var oldTooltip = option.title;
-						var newTooltip = 'Original value: “' + option.getAttribute('value') + '”';
-
-						option.title = oldTooltip
-							? oldTooltip + '\n\n' + newTooltip
-							: newTooltip;
-					}
-				});
-
-				/* Allow changing OPTION values. */
-				element.addEventListener('change', function (event) {
-					var option = element.options[element.selectedIndex];
-					var newValue = prompt('New value for option ' + element.selectedIndex + ' (“' + (option.textContent || option.value) + '”):', option.value);
-					if (newValue !== null) {
-						if (newValue !== option.value) {
-							option.textContent = newValue;
-						}
-
-						option.value = newValue;
-					}
-				});
-			}
-
-			/* Create the new tooltip. */
-			if (tooltipLines.length) {
-				var oldTooltip = element.title;
-
-				var newTooltip = tooltipLines.join('\n');
-
-				element.title = oldTooltip
-					? oldTooltip + '\n\n' + newTooltip
-					: newTooltip;
 			}
 		});
+
+		/* Remove unwanted attributes. */
+		attributeNamesToRemove.forEach(function (attrName) {
+			if (element.hasAttribute(attrName) || element[attrName]) {
+				console.log('Freeform: remove “' + attrName + '” attribute on element: ', element);
+				tooltipLines.push('Removed “' + attrName + '” attribute; was: “' + (element.getAttribute(attrName) || element[attrName]) + '”');
+
+				delete element[attrName];
+				element.removeAttribute(attrName);
+			}
+		});
+
+		if (element.tagName.toUpperCase() === 'INPUT') {
+			if (element.hasAttribute('type')) {
+				var type = element.getAttribute('type').toLowerCase();
+				/* Change unwanted INPUT types to default INPUTs. */
+				if (type !== '' && allowedInputTypes.indexOf(type) === -1) {
+					console.log('Freeform: remove “type” attribute on element: ', element);
+					tooltipLines.push('Removed “type” attribute; was: “' + element.getAttribute('type') + '”');
+
+					element.removeAttribute('type');
+				}
+
+				/* Remove MIME type restriction on file uploads. */
+				if (type === 'file') {
+					element.removeAttribute('accept');
+				}
+			}
+
+			/* Change all default (text) INPUTs to multiline TEXTAREAs. */
+			if (!element.hasAttribute('type')) {
+				var textarea = element.ownerDocument.createElementNS('http://www.w3.org/1999/xhtml', 'textarea');
+
+				textarea.name = element.name;
+				textarea.value = element.value;
+				textarea.rows = 1;
+
+				if (element.placeholder) {
+					textarea.placeholder = element.placeholder;
+				}
+
+				if (element.id) {
+					textarea.id = element.id;
+				}
+
+				element.parentNode.replaceChild(textarea, element);
+				element = textarea;
+
+				var computedStyle = getComputedStyle(textarea);
+				textarea.style.height = textarea.style.minHeight =
+					parseInt(computedStyle.paddingTop || 0, 10)
+					+ parseInt(computedStyle.lineHeight || 0, 10)
+					+ parseInt(computedStyle.paddingBottom || 0, 10)
+					+ 'px';
+
+				tooltipLines.push('Changed from INPUT to TEXTAREA');
+			}
+		} else if (element.tagName.toUpperCase() === 'SELECT') {
+			/* Make sure there is at least one option to change. */
+			element.insertBefore(document.createElementNS('http://www.w3.org/1999/xhtml', 'option'), element.firstChild);
+
+			/* Put the original OPTION value in the tooltip. */
+			[].forEach.call(element.options, function (option) {
+				if (option.hasAttribute('value')) {
+					var oldTooltip = option.title;
+					var newTooltip = 'Original value: “' + option.getAttribute('value') + '”';
+
+					option.title = oldTooltip
+						? oldTooltip + '\n\n' + newTooltip
+						: newTooltip;
+				}
+			});
+
+			/* Allow changing OPTION values. */
+			element.addEventListener('change', function (event) {
+				var option = element.options[element.selectedIndex];
+				var newValue = prompt('New value for option ' + element.selectedIndex + ' (“' + (option.textContent || option.value) + '”):', option.value);
+				if (newValue !== null) {
+					if (newValue !== option.value) {
+						option.textContent = newValue;
+					}
+
+					option.value = newValue;
+				}
+			});
+		}
+
+		/* Create the new tooltip. */
+		if (tooltipLines.length) {
+			var oldTooltip = element.title;
+
+			var newTooltip = tooltipLines.join('\n');
+
+			element.title = oldTooltip
+				? oldTooltip + '\n\n' + newTooltip
+				: newTooltip;
+		}
 	});
 
 	/* Recurse for frames and iframes. */
