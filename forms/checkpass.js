7092c085047236b059d9ea5c7612fdff63a3006a
Jan Moesen <github.com@moesen.nu>
2020-02-07 22:13:17 +0100

    checkpass: use `INPUT@autocomplete="new-password"`
    See https://html.spec.whatwg.org/multipage/form-control-infrastructure.html#attr-fe-autocomplete-new-password
    and https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/autocomplete#Values
    for more information.
    
    (Ugh, I forgot to regenerate bookmarks.html after the previous commit
    for 2en, 2nl, … Sorry for the ugly noise in this commit.)

--- forms/checkpass.js
+++ forms/checkpass.js
@@ -10,20 +10,29 @@
 	function execute(document) {
 		let needsStyleSheet = false;
 
-		Array.from(document.querySelectorAll('form input[type="password"]')).forEach(input => {
+		Array.from(document.querySelectorAll('form input[type="password"], form input[autocomplete="new-password"]')).forEach(input => {
 			if (input.form.xxxJanHasCheckpass) {
 				return;
 			}
 
-			/* Make sure there are at least two password INPUTs. In case
-			 * there are more than two, we assume only the last two to be
+			/* Make sure there are at least two password INPUTs. If there
+			 * are two inputs (regardless of their INPUT@type) having the
+			 * INPUT@autocomplete attribute set to `new-password`, we use
+			 * those two.
+			 *
+			 * Otherwise, look for inputs whose INPUT@type is `password`.
+			 * If there are more than two, assume only the last two to be
 			 * “linked”. For instance, on a form to change your password,
 			 * there are typically three password INPUTs: your current
 			 * password, the new password, and a confirmation for the new
 			 * password. On a registration form, there are typically just
 			 * two password INPUTs, one for the password, and one for the
 			 * confirmation. */
-			let allPasswordInputs = Array.from(input.form.querySelectorAll('input[type="password"]'));
+			let allPasswordInputs = Array.from(input.form.querySelectorAll('input[autocomplete="new-password"]'));
+			if (allPasswordInputs.length < 2) {
+				allPasswordInputs = Array.from(input.form.querySelectorAll('input[type="password"]'));
+			}
+
 			if (allPasswordInputs.length < 2) {
 				return;
 			}
