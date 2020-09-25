666d0cfb7bf198bff589834a408dc70f97321519
Jan Moesen <github.com@moesen.nu>
2020-09-25 17:30:05 +0200

    yttop: initial commit (search YouTube music)
    Search YouTube for the specified or selected text inside “topic” videos.

---
+++ search/yttop.js
@@ -0,0 +1,76 @@
+/**
+ * Search YouTube for the specified or selected text inside “topic” videos.
+ *
+ * “Topic” videos are the official channels for artists/bands/musicians and can
+ * be recognized by either “ - Topic” or “🎵” suffixed to the name. However, it
+ * is easier to search for the text “Provided to YouTube by”, which is at the
+ * start of the auto-generated description for music “videos”.
+ *
+ * @title YouTube topic search
+ */
+(function yt() {
+	/* Try to get the parameter string from the bookmarklet/search query.
+	   Fall back to the current text selection, if any. If those options
+	   both fail, prompt the user.
+	*/
+	var s = (function () { /*%s*/; }).toString()
+		.replace(/^function\s*\(\s*\)\s*\{\s*\/\*/, '')
+		.replace(/\*\/\s*\;?\s*\}\s*$/, '')
+		.replace(/\u0025s/, '');
+
+	/**
+	 * Get the active text selection, diving into frames and
+	 * text controls when necessary and possible.
+	 */
+	function getActiveSelection(doc) {
+		if (arguments.length === 0) {
+			doc = document;
+		}
+
+		if (!doc || typeof doc.getSelection !== 'function') {
+			return '';
+		}
+
+		if (!doc.activeElement) {
+			return doc.getSelection() + '';
+		}
+
+		var activeElement = doc.activeElement;
+
+		/* Recurse for FRAMEs and IFRAMEs. */
+		try {
+			if (
+				typeof activeElement.contentDocument === 'object'
+				&& activeElement.contentDocument !== null
+			) {
+				return getActiveSelection(activeElement.contentDocument);
+			}
+		} catch (e) {
+			return doc.getSelection() + '';
+		}
+
+		/* Get the selection from inside a text control. */
+		if (typeof activeElement.value === 'string') {
+			if (activeElement.selectionStart !== activeElement.selectionEnd) {
+				return activeElement.value.substring(activeElement.selectionStart, activeElement.selectionEnd);
+			}
+
+			return activeElement.value;
+		}
+
+		/* Get the normal selection. */
+		return doc.getSelection() + '';
+	}
+
+	if (s === '') {
+		s = getActiveSelection() || prompt('Please enter your YouTube topic search query:');
+	} else {
+		s = s.replace(/(^|\s|")~("|\s|$)/g, '$1' + getActiveSelection() + '$2');
+	}
+
+	if (s) {
+		s = s.replace(/\uFEFF/g, '');
+
+		location = 'https://www.youtube.com/results?search_query=' + encodeURIComponent(s + ' "Provided to YouTube by"');
+	}
+})();
