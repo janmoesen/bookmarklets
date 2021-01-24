9235d29923551aed989f9f97d7e393dee7d7077c
Jan Moesen <github.com@moesen.nu>
2021-01-24 23:17:08 +0100

    mapi: initial commit for Mapillary

---
+++ search/maps/mapi.js
@@ -0,0 +1,97 @@
+/**
+ * Go to the specified (lat, lng) coordinates with Mapillary.
+ *
+ * @title Mapillary
+ */
+(function mapi(document) {
+	'use strict';
+
+	/* Create a new IFRAME to get a "clean" Window object, so we can use its
+	 * console. Sometimes sites (e.g. Twitter) override console.log and even
+	 * the entire console object. "delete console.log" or "delete console"
+	 * does not always work, and messing with the prototype seemed more
+	 * brittle than this. */
+	let console = (function () {
+		let iframe = document.getElementById('xxxJanConsole');
+		if (!iframe) {
+			iframe = document.createElementNS('http://www.w3.org/1999/xhtml', 'iframe');
+			iframe.id = 'xxxJanConsole';
+			iframe.style.display = 'none';
+
+			(document.body || document.documentElement).appendChild(iframe);
+		}
+
+		return iframe && iframe.contentWindow && iframe.contentWindow.console || {
+			log: function () {}
+		};
+	})();
+
+	/* Try to get the parameter string from the bookmarklet/search query.
+	 * Fall back to the current text selection, if any. If those options
+	 * both fail, prompt the user. */
+	let s = (function () { /*%s*/; }).toString()
+		.replace(/^function\s*\(\s*\)\s*\{\s*\/\*/, '')
+		.replace(/\*\/\s*\;?\s*\}\s*$/, '')
+		.replace(/\u0025s/, '');
+
+	/**
+	 * Get the active text selection, diving into frames and
+	 * text controls when necessary and possible.
+	 */
+	function getActiveSelection(document) {
+		if (!document || typeof document.getSelection !== 'function') {
+			return '';
+		}
+
+		if (!document.activeElement) {
+			return document.getSelection() + '';
+		}
+
+		const activeElement = document.activeElement;
+
+		/* Recurse for FRAMEs and IFRAMEs. */
+		try {
+			if (
+				typeof activeElement.contentDocument === 'object'
+				&& activeElement.contentDocument !== null
+			) {
+				return getActiveSelection(activeElement.contentDocument);
+			}
+		} catch (e) {
+			return document.getSelection() + '';
+		}
+
+		/* Get the selection from inside a text control. */
+		if (typeof activeElement.value === 'string') {
+			if (activeElement.selectionStart !== activeElement.selectionEnd) {
+				return activeElement.value.substring(activeElement.selectionStart, activeElement.selectionEnd);
+			}
+
+			return activeElement.value;
+		}
+
+		/* Get the normal selection. */
+		return document.getSelection() + '';
+	}
+
+	if (s === '') {
+		s = getActiveSelection(document) || prompt('Specify the (lat, lng) coordinates:');
+	} else {
+		s = s.replace(/(^|\s|")~("|\s|$)/g, '$1' + getActiveSelection(document) + '$2');
+	}
+
+	if (s) {
+		const [lat, lng] = s
+			.replace(/^\s*\(\s*/, '')
+			.replace(/\s*\(\s*$/, '')
+			.split(/\s*[ ,]\s*/)
+			.map(parseFloat);
+
+		if (typeof lat !== 'number' || typeof lng !== 'number') {
+			alert(`Could not parse (lat, lng) coordinates from “${s}”`);
+			return;
+		}
+
+		location = `https://www.mapillary.com/app/?lat=${encodeURIComponent(lat)}&lng=${encodeURIComponent(lng)}&z=13`;
+	}
+})(document);
