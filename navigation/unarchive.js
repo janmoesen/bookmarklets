1e5ef725381aa973ff4bb86ec66d55d15821c51d
Jan Moesen <jan@tervelo.com>
2021-09-24 13:19:22 +0200

    unarchive: use `setAttributeNS(href, …)`

--- navigation/unarchive.js
+++ navigation/unarchive.js
@@ -1,6 +1,6 @@
 /**
  * Rewrite links to the Internet Archive Wayback Machine’s saved pages back to
- * the original URLs.
+ * the original URIs.
  *
  * @title Unarchive Wayback Machine links
  */
@@ -9,20 +9,52 @@
 
 	/* Recursively execute the logic on the document and its sub-documents. */
 	function execute(document) {
-		/* Bypass the attribute setter from Wombat (the Wayback Machine’s JS library) that dynamically rewrites URLs. */
-		const setAttrMethod = document.defaultView.Element.prototype._orig_setAttribute
-			? document.defaultView.Element.prototype._orig_setAttribute
-			: document.defaultView.Element.prototype.setAttribute;
+		const isOnArchiveDotOrg = document.location.host === 'web.archive.org';
+		let wbASelectors;
 
-		Array.from(document.querySelectorAll('a[href^="//web.archive.org/web/"], a[href^="http://web.archive.org/web/"], a[href^="https://web.archive.org/web/"]')).forEach(
-			a => setAttrMethod.call(a, 'href', a.getAttribute('href').replace(/^(https?:)?\/\/web\.archive\.org\/.*?(https?:\/\/)/, '$2'))
-		);
-
-		if (document.location.host === 'web.archive.org') {
-			Array.from(document.querySelectorAll('a[href^="/web/"][href*="://"]')).forEach(
-				a => a.setAttribute('href', a.getAttribute('href').replace(/^\/web\/.*?(https?:\/\/)/, '$1'))
-			);
+		if (isOnArchiveDotOrg) {
+			wbASelectors = [
+				/* The Wayback Machine does not rewrite *all* HREF attributes
+				 * in the HTML source. Relative HREFs are left alone, e.g. on
+				 * <https://web.archive.org/web/20210118051332/http://www.w3.org/2006/11/mwbp-tests/index.xhtml>:
+				 *
+				 * `<a href="https://web.archive.org/web/20210118051332/http://www.w3.org/2005/MWI/BPWG/techs/EncodingDeclarationSupport">`
+				 *
+				 * vs.
+				 *
+				 * `<a href="test-text-html.html">`
+				 *
+				 * So we have to check the HREF for *every* A element to get its
+				 * resolved URI.
+				 */
+				'a[href]'
+			];
+		} else {
+			wbASelectors = [
+				'a[href^="//web.archive.org/web/"]',
+				'a[href^="http://web.archive.org/web/"]',
+				'a[href^="https://web.archive.org/web/"]'
+			];
 		}
+
+		/* Wombat (the Wayback Machine’s JS library) dynamically rewrites URIs by
+		 * intercepting calls to `{get,set}Attribute('href')` and the `a.href` getter
+		 * and setter. However, calls to `setAttributeNS` are *not* intercepted. Take
+		 * advantage of the `a.href` interceptor that returns the resolved original URI
+		 * and use `setAttributeNS` to override the Wayback Machine’s rewritten HREF.
+		 *
+		 * On pages outside of web.archive.org, `a.href` returns the full archive.org
+		 * URI, so we have to strip it ourselves.
+		 */
+		Array.from(document.querySelectorAll(wbASelectors.join(', '))).forEach(a => {
+			let originalUri = a.href;
+
+			if (!isOnArchiveDotOrg) {
+				originalUri = originalUri.replace(/^https?\:\/\/web.archive\.org\/web\/([^\/]+\/)?(https?:\/\/.*)/, '$2');
+			}
+
+			a.setAttributeNS('', 'href', originalUri);
+		});
 
 		/* Recurse for frames and IFRAMEs. */
 		try {
