d189d80e2ec37ec301ec8908eda6865554e42815
Jan Moesen <jan@tervelo.com>
2021-10-13 22:05:03 +0200

    vidspeed/ffwd: indicate current media speed
    With some fancy-pants animation, even. Ahem.

--- content/vidspeed.js
+++ content/vidspeed.js
@@ -10,7 +10,7 @@
 	function getMedia(document) {
 		let allMedia = Array.from(document.querySelectorAll('video, audio'));
 
-		/* Find video and audio inside shadow DOMs. */
+		/* Find video and audio inside the shadow DOM of custom elements (Web Components). */
 		const notRegularHtmlElementsSelector = 'a,abbr,address,area,article,aside,audio,b,base,bdi,bdo,blockquote,body,br,button,canvas,caption,cite,code,col,colgroup,data,datalist,dd,del,details,dfn,dialog,div,dl,dt,em,embed,fieldset,figcaption,figure,footer,form,h1,h2,h3,h4,h5,h6,head,header,hgroup,hr,html,i,iframe,img,input,ins,kbd,label,legend,li,link,main,map,mark,math,math *,menu,meta,meter,nav,noscript,object,ol,optgroup,option,output,p,param,picture,pre,progress,q,rp,rt,ruby,s,samp,script,section,select,slot,small,source,span,strong,style,sub,summary,sup,svg,svg *,table,tbody,td,template,textarea,tfoot,th,thead,time,title,tr,track,u,ul,var,video,wbr'
 			.split(',')
 			.map(s => `:not(${s})`)
@@ -53,6 +53,47 @@
 	}
 
 	if (s) {
-		allMedia.forEach(media => media.playbackRate = s);
+		allMedia.forEach(media => {
+			media.playbackRate = s;
+
+			/* Indicate the new playback rate (speed) for the media element. */
+			let visibleMediaContainer = media;
+			let rect = visibleMediaContainer.getBoundingClientRect();
+			while ((rect.height <= 0 || rect.width <= 0) && (visibleMediaContainer = visibleMediaContainer.parentNode)) {
+				rect = visibleMediaContainer.getBoundingClientRect();
+			}
+
+			const indicator = document.createElement('div');
+			indicator.textContent = media.playbackRate + '×';
+			indicator.setAttribute('style', `
+				display: flex;
+				position: fixed;
+				left: ${Math.max(0, rect.left)}px;
+				top: ${Math.max(0, rect.top)}px;
+				z-index: 10000;
+				width: ${rect.width}px;
+				height: ${rect.height}px;
+				font-size: ${rect.height / 10}px;
+				justify-content: center;
+				align-items: center;
+				color: rgba(0, 0, 0, 1);
+				text-shadow: 0 0 10px rgba(255, 255, 255, 1);
+				transition: all 0.75s ease-out;
+				pointer-events: none;
+			`.replace(/^\s*|\s*$/gm, ''));
+
+			document.body.appendChild(indicator);
+
+			/* Animate and remove the indicator. */
+			setTimeout(_ => {
+				setTimeout( _ => indicator.remove(), 750);
+
+				indicator.setAttribute('style', indicator.getAttribute('style') + `
+					font-size: ${rect.height}px;
+					color: rgba(0, 0, 0, 0);
+					text-shadow: 0 0 10px rgba(255, 255, 255, 0);
+				`.replace(/^\s*|\s*$/gm, ''));
+			}, 0);
+		});
 	}
 })();
