2f129f86d7643ba0fcf8f04a261017b86656723f
Jan Moesen <jan@tervelo.com>
2022-09-19 14:49:40 +0200

    kbo: if multiple VAT-like numbers, choose the best

--- search/kbo.js
+++ search/kbo.js
@@ -71,21 +71,36 @@
 	}
 
 	if (s) {
-		let matches;
-		if ((matches = s.match(/.*\b(BE)?\s?(\d{3})[ .-]?(\d{3})[ .-]?(\d{3})\b.*/i))) {
+		const possibleVatNumbers = [];
+		const regexp = /\b(?<bePrefix>BE)?\s?(?<leadingZero>0)?(?<p1>\d{3})([ .-]?)(?<p2>\d{3})\4(?<p3>\d{3})\b/gi;
+		for (const matches of s.matchAll(regexp)) {
 			/* This looks like a Belgian company number. */
+			possibleVatNumbers.push({
+				hasBePrefix: !!matches.groups.bePrefix,
+				hasLeadingZero: matches.groups.leadingZero === '0',
+				vatNumber: (matches.groups.leadingZero || '') + matches.groups.p1 + matches.groups.p2 + matches.groups.p3,
+				matches
+			});
+		}
+
+		if (possibleVatNumbers.length) {
+			const bestMatch =
+				possibleVatNumbers.find(possibleVatNumber => possibleVatNumber.hasBePrefix)
+				/* Numbers starting with `04` are often just telephone numbers, e.g. `0479 000 000`. */
+				|| possibleVatNumbers.find(possibleVatNumber => possibleVatNumber.hasLeadingZero && !possibleVatNumber.vatNumber.match(/^04/))
+				|| possibleVatNumbers[0];
+
 			location = 'https://kbopub.economie.fgov.be/kbopub/toonondernemingps.html?ondernemingsnummer='
-				+ encodeURIComponent(matches[2] + matches[3] + matches[4]);
+				+ encodeURIComponent(bestMatch.vatNumber);
+
 		} else {
 			let normalizedS = s.trim().replaceAll(/ (bus|boîte|boite) /gi, 'b').replaceAll(/\s+/g, ' ');
-			if (
-				(matches = normalizedS.match(/(?<street>.{3,63}[^0-9]),? (?<number>\d+[^,]{0,5})[,/-]? (?<postalCode>[1-9]\d\d\d)\b/))
-				|| (matches = normalizedS.match(/(?<number>\d+[^,]{0,5}),? (?<street>.{3,63}[^0-9]),? (?<postalCode>[1-9]\d\d\d)\b/))
-			) {
+			let matches = normalizedS.match(/(?<street>.{3,63}[^0-9]),? (?<number>\d+[^,]{0,5})[,/-]? (?<postalCode>[1-9]\d\d\d)\b/)
+				|| normalizedS.match(/(?<number>\d+[^,]{0,5}),? (?<street>.{3,63}[^0-9]),? (?<postalCode>[1-9]\d\d\d)\b/);
+
+			if (matches) {
 				/* This looks like a Belgian address. */
 				location = `https://kbopub.economie.fgov.be/kbopub/zoekadresform.html?filterEnkelActieve=false&actionLU=Zoek&postcod1=${encodeURIComponent(matches.groups.postalCode)}&postgemeente1=&straatgemeente1=${encodeURIComponent(matches.groups.street)}&huisnummer=${encodeURIComponent(matches.groups.number)}`;
-				console.log('Yay! ', matches.groups);
-				console.log('→ ', location);
 				return;
 			}
 
