d57776b836aaeb7c6c904bfe2dc956a471fd6eed
Jan Moesen <jan@tervelo.com>
2023-03-22 12:21:50 +0100

    2comm: initial commit (go to Wikimedia Commons)

---
+++ search/wikipedia/2comm.js
@@ -0,0 +1,33 @@
+/**
+ * Go to the corresponding Wikimedia Commons page for a Wikipedia article.
+ *
+ * @title Go to Wikimedia Commons
+ * @keyword 2comm
+ */
+(function() {
+	'use strict';
+
+	/* Recursively execute the logic on the document and its sub-documents. */
+	function execute(document) {
+		const commonsLink = document.querySelector('.wb-otherproject-link.wb-otherproject-commons a[href]');
+		if (commonsLink) {
+			try {
+				top.location = commonsLink.href;
+				return;
+			} catch (e) {
+				location = commonsLink.href;
+			}
+		}
+
+		/* Recurse for (i)frames. */
+		try {
+			Array.from(document.querySelectorAll('frame, iframe, object[type^="text/html"], object[type^="application/xhtml+xml"]')).forEach(
+				elem => { try { execute(elem.contentDocument) } catch (e) { } }
+			);
+		} catch (e) {
+			/* Catch and ignore exceptions for out-of-domain access. */
+		}
+	}
+
+	execute(document);
+})();
