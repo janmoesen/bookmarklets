b3fa283e64ebac844e377ee54c08d98c913d860b
Jan Moesen <jan@tervelo.com>
2023-06-01 22:50:04 +0200

    Maps: support multilingual N/S/W/E notations

--- search/maps/bmap.js
+++ search/maps/bmap.js
@@ -64,6 +64,191 @@
 	}
 
 	if (s) {
+		/* TODO: get the anchor node from the same selection as used by
+		 * getActiveSelection(). The following code only looks at the top
+		 * document, even though it might look fancier than that. */
+		let anchorNode = getSelection()?.anchorNode;
+		while (anchorNode && !(anchorNode instanceof HTMLElement)) {
+			anchorNode = anchorNode.parentNode;
+		}
+		const selectedLanguage = (anchorNode?.closest('[lang], [xml\\:lang]') || document.documentElement)?.lang;
+
+		/* Rewrite non-English coordinates as used on Wikipedia to the
+		 * English version that Google Maps can parse. Examples below.
+		 */
+		const localizedCoordinatesLabels = {
+			/* English is the default, but if the selection’s language can
+			 * be determined, that language will have precedence. */
+			en: {
+				/* https://en.wikipedia.org/wiki/New_York_City
+				 → Before: 40°42′46″N 74°00′22″W
+				 ← After:  40°42′46″ N, 74°00′22″ W
+
+				 * https://en.wikipedia.org/wiki/Sydney
+			      ← Before: 33°52′04″S 151°12′36″E
+			      ← After:  33°52′04″ S, 151°12′36″ E */
+				N: 'N',
+				S: 'S',
+				W: 'W',
+				E: 'E',
+			},
+
+			nl: {
+				/* https://nl.wikipedia.org/wiki/New_York_(stad)
+				 → Before: 40° 43′ NB, 74° 0′ WL
+				 ← After:  40° 43′ N, 74° 0′ W
+
+				 * https://nl.wikipedia.org/wiki/Sydney
+				 → Before: 33° 52′ ZB, 151° 13′ OL
+				 ← After:  33° 52′ S, 151° 13′ E */
+				N: 'NB',
+				S: 'ZB',
+				W: 'WL',
+				E: 'OL',
+			},
+
+			fr: {
+				/* https://fr.wikipedia.org/wiki/New_York
+				 → Before: 40° 42′ nord, 74° 00′ ouest
+				 ← After:  40° 42′ N, 74° 00′ W
+
+				 * https://fr.wikipedia.org/wiki/Sydney
+				 → Before: 33° 51′ 22″ sud, 151° 11′ 33″ est
+				 ← After:  33° 51′ 22″ S, 151° 11′ 33″ E */
+				N: 'nord',
+				S: 'sud',
+				W: 'ouest',
+				E: 'est',
+			},
+
+			de: {
+				/* https://de.wikipedia.org/wiki/New_York_City
+				 → Before: 40° 43′ N, 74° 0′ W
+				 ← After:  40° 43′ N, 74° 0′ W
+
+				 * https://de.wikipedia.org/wiki/Sydney
+				 → Before: 33° 51′ S, 151° 12′ O
+				 ← After:  33° 51′ S, 151° 12′ E */
+				N: 'N',
+				S: 'S',
+				W: 'W',
+				E: 'O',
+			},
+
+			es: {
+				/* https://es.wikipedia.org/wiki/Nueva_York
+				 → Before: 40°42′N 74°00′O
+				 ← After:  40°42′ N, 74°00′ W
+
+				 * https://es.wikipedia.org/wiki/S%C3%ADdney
+				 → Before: 33°52′04″S 151°12′36″E
+				 ← After:  33°52′04″ S, 151°12′36″ E */
+				N: 'N',
+				S: 'S',
+				W: 'O',
+				E: 'E',
+			},
+
+			vi: {
+				/* https://vi.wikipedia.org/wiki/Th%C3%A0nh_ph%E1%BB%91_New_York
+				 → Before: 40°42′46″B 74°00′22″T
+				 ← After:  40°42′46″ N, 74°00′22″ W
+
+				 * https://vi.wikipedia.org/wiki/Sydney
+				 → Before: 33°51′35,9″N 151°12′40″Đ
+				 ← After:  33°51′35.9″S, 151°12′40″E */
+				N: 'B',
+				S: 'N',
+				W: 'T',
+				E: 'Đ',
+			},
+
+			ru: {
+				/* https://ru.wikipedia.org/wiki/%D0%9D%D1%8C%D1%8E-%D0%99%D0%BE%D1%80%D0%BA
+				 → Before: 40°43′42″ с. ш. 73°59′39″ з. д.
+				 ← After:  40°43′42″ N, 73°59′39″ W
+
+				 * https://ru.wikipedia.org/wiki/%D0%A1%D0%B8%D0%B4%D0%BD%D0%B5%D0%B9
+				 → Before: 33°52′10″ ю. ш. 151°12′30″ в. д.
+				 ← After:  33°51′35.9″ S, 151°12′40″ E */
+				N: 'с. ш.',
+				S: 'ю. ш.',
+				W: 'з. д.',
+				E: 'в. д.',
+			},
+		};
+
+		const possibleReplacements = [];
+		for (const [language, labels] of Object.entries(localizedCoordinatesLabels)) {
+			/* Prefer the N/S/W/E labels for the selected element’s language. For
+			* instance, don’t assume “N” always means “North”; in Vietnamese it
+			* stands for “Nam”, meaning “South”. */
+			const method = language === selectedLanguage
+				? 'unshift'
+				: 'push';
+
+			possibleReplacements[method]({language, labels});
+		}
+
+		const regexpWithPlaceholders = /(?<latitude>\d+\s*°(?:\s*\d+\s*′(?:\s*\d+(?:[.,]\d+)?\s*(?:′′|″))?)?)\s*(?<latitudeLabel>XXX_LAT_XXX)\s*,?\s*(?<longitude>\d+\s*°(?:\s*\d+\s*′(?:\s*\d+(?:[.,]\d+)?\s*(?:′′|″))?)?)\s*(?<longitudeLabel>XXX_LNG_XXX)/;
+
+		const stringRegexpWithPlaceholders = regexpWithPlaceholders
+			.toString()
+			.replace(/(^\/)|(\/$)/g, '');
+
+		const rewrittenCoordinates = new Set();
+		for (let {language, labels} of possibleReplacements) {
+			let hasReplaced = false;
+
+			const regexp = new RegExp(
+				stringRegexpWithPlaceholders
+					.replace('XXX_LAT_XXX', `${labels.N}|${labels.S}`)
+					.replace('XXX_LNG_XXX', `${labels.W}|${labels.E}`),
+				'g'
+			);
+
+			s = s.replaceAll(regexp, (...arguments) => {
+				const matchedGroups = arguments.pop();
+
+				/* We found a coordinate pair using the labels for the current
+				 * language. Change it to the English notation. */
+				hasReplaced = true;
+				const rewrittenCoordinate = ''
+					+ matchedGroups.latitude.replace(/,/g, '.')
+					+ ' '
+					+ (matchedGroups.latitudeLabel === labels.N
+						? 'N'
+						: 'S')
+					+ ', '
+					+ matchedGroups.longitude.replace(/,/g, '.')
+					+ ' '
+					+ (matchedGroups.longitudeLabel === labels.W
+						? 'W'
+						: 'E');
+
+				rewrittenCoordinates.add(rewrittenCoordinate);
+
+				return rewrittenCoordinate;
+			});
+
+			if (hasReplaced) {
+				/* If we found a match for one language, skip all the others to avoid unintended effects. E.g. N/W/S/E is English */
+				break;
+			}
+		}
+
+		/* If we found one or more coordinate pairs in the whole text, only
+		 * search for those. That makes it more user-friendly on sites like
+		 * Wikipedia where it is hard to select only the coordinates and not
+		 * leading text like “Coordinates:” or trailing utility links like
+		 * “[mappa]” or “(GeoHackFoo)”. */
+		if (rewrittenCoordinates.size) {
+			/* Note that currently (2023-06-01) Bing Maps does not support directions
+			 * between coordinates in this way. It does support directions from a
+			 * coordinate pair to a city name, for instance. */
+			s = Array.from(rewrittenCoordinates).join(' to ');
+		}
+
 		location = 'https://www.bing.com/maps/?obox=1&q=' + encodeURIComponent(s)
 			.replace(/%20/g, '+')
 			.replace(/%3A/g, ':')
