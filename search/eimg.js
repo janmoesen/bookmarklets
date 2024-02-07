731e323b7fca7b7d9868008a9d018a77ab704fc8
Jan Moesen <jan@tervelo.com>
2024-02-07 16:47:07 +0100

    e/eimg: add Ecosia search engine (initial commit)
    It uses Bing as its provider, so the results are the same as DuckDuckGo,
    and it has a similar approach to privacy. But Ecosia plants trees (which
    seems to be more than just greenwashing), so I think I prefer them now.

---
+++ search/eimg.js
@@ -0,0 +1,66 @@
+/**
+ * Search using Ecosia Images, for “A better planet with every search”.
+ *
+ * @title Ecosia Images
+ */
+(function eimg(document) {
+	'use strict';
+
+	/* Try to get the parameter string from the bookmarklet/search query.
+	 * Fall back to the current text selection, if any. If those options
+	 * both fail, prompt the user. */
+	let s = (function () { /*%s*/; }).toString()
+		.replace(/^function\s*\(\s*\)\s*\{\s*\/\*/, '')
+		.replace(/\*\/\s*\;?\s*\}\s*$/, '')
+		.replace(/\u0025s/, '');
+
+	/**
+	 * Get the active text selection, diving into frames and
+	 * text controls when necessary and possible.
+	 */
+	function getActiveSelection(document) {
+		if (!document || typeof document.getSelection !== 'function') {
+			return '';
+		}
+
+		if (!document.activeElement) {
+			return document.getSelection() + '';
+		}
+
+		const activeElement = document.activeElement;
+
+		/* Recurse for FRAMEs and IFRAMEs. */
+		try {
+			if (
+				typeof activeElement.contentDocument === 'object'
+				&& activeElement.contentDocument !== null
+			) {
+				return getActiveSelection(activeElement.contentDocument);
+			}
+		} catch (e) {
+			return document.getSelection() + '';
+		}
+
+		/* Get the selection from inside a text control. */
+		if (typeof activeElement.value === 'string') {
+			if (activeElement.selectionStart !== activeElement.selectionEnd) {
+				return activeElement.value.substring(activeElement.selectionStart, activeElement.selectionEnd);
+			}
+
+			return activeElement.value;
+		}
+
+		/* Get the normal selection. */
+		return document.getSelection() + '';
+	}
+
+	if (s === '') {
+		s = getActiveSelection(document) || prompt('Please enter your Ecosia image search query:');
+	} else {
+		s = s.replace(/(^|\s|")~("|\s|$)/g, '$1' + getActiveSelection(document) + '$2');
+	}
+
+	if (s) {
+		location = 'https://www.ecosia.org/images?q=' + encodeURIComponent(s);
+	}
+})(document);
