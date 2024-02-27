ae40f6388c7a09662d3823c61cf74ad9d706c4ec
Jan Moesen <jan@tervelo.com>
2024-02-27 18:14:20 +0100

    reconsole: use `console` from new IFRAME

--- development/reconsole.js
+++ development/reconsole.js
@@ -4,5 +4,15 @@
  * @title Re-console
  */
 (function reconsole() {
+	'use strict';
+
+	/* Sometimes all it takes to revert back to the original `console`, is to
+	 * delete the imposter. */
 	delete window.console;
+
+	/* But if that didn’t work, create an IFRAME and use its `console`. */
+	if (!window.console) {
+		const iframe = document.body.appendChild(document.createElement('iframe'));
+		window.console = iframe.contentWindow.console;
+	}
 })();
